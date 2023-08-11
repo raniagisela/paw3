58f45b14930f54f4c6e4ddd1d5033b4a678f55c6
raniagisela <raniagslp@gmail.com>
2023-08-11 21:32:16 +0700

    commit

---
+++ src/main/java/com/example3/BelajarOOP/Person.java
@@ -0,0 +1,16 @@
+package com.example3.BelajarOOP;
+
+public class Person {
+    String name;
+    String address;
+    final String country = "Indonesia";
+
+    Person(String paramName, String paramAddress) {
+        name = paramName;
+        address = paramAddress;
+    }
+    
+    void sayHello(String paramName) {
+        System.out.println("Hello " + paramName + ", My Name is " + name);
+    }
+}
